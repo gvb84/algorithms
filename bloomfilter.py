04b0d6c8c8526d1316bea1896caa1378e14fa134
Vincent Berg <gvb@santarago.org>
2018-07-23 14:26:12 +0200

    fixed typo in license declarations

--- bloomfilter.py
+++ bloomfilter.py
@@ -18,7 +18,7 @@ The hash function used is SipHash-2-4 but this can easily be changed.
 "THE BEER-WARE LICENSE" (Revision 42):
 Vincent Berg <gvb@santarago.org> wrote this file.  As long as you retain this
 notice you can do whatever you want with this stuff. If we meet some day, and
-you think this stuff is worth it, you can buy me a beer in retur
+you think this stuff is worth it, you can buy me a beer in return.
 """
 
 from math import log, ceil
--- siphash.py
+++ siphash.py
@@ -11,7 +11,7 @@ job done.
 "THE BEER-WARE LICENSE" (Revision 42):
 Vincent Berg <gvb@santarago.org> wrote this file.  As long as you retain this
 notice you can do whatever you want with this stuff. If we meet some day, and
-you think this stuff is worth it, you can buy me a beer in retur
+you think this stuff is worth it, you can buy me a beer in return.
 """
 
 
